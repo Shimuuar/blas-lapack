771ae3215f67e08928efd7d4f122567293976bc5
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2012-08-02 21:44:04 +0400

    Add some bindings to level 1 BLAS

---
+++ Numeric/BLAS.hs
@@ -0,0 +1,46 @@
+module Numeric.BLAS (
+    -- * Vector operations
+    dotProduct
+  , hermitianProd
+  , vectorNorm
+  ) where
+
+import Control.Monad.Primitive
+import Control.Monad.ST
+
+import qualified Numeric.BLAS.Bindings as BLAS
+import           Numeric.BLAS.Bindings   (BLAS1,BLAS2,BLAS3)
+
+import Numeric.BLAS.Vector
+
+-- | Scalar product of vectors
+dotProduct :: BLAS1 a => Vector a -> Vector a -> a
+{-# INLINE dotProduct #-}
+dotProduct v w
+  = runST
+  $ unsafeIOToST
+  $ unsafeWithVector v $ \n s1 p ->
+    unsafeWithVector w $ \m s2 q ->
+    case n == m of
+      True  -> BLAS.dotu n p s1 q s2
+      False -> error "DOTC"
+    
+-- | hermitian dot product of vectors. For real-valued vectors is same
+--   as 'dotProduct'.
+hermitianProd :: BLAS1 a => Vector a -> Vector a -> a
+hermitianProd v w
+  = runST
+  $ unsafeIOToST
+  $ unsafeWithVector v $ \n s1 p ->
+    unsafeWithVector w $ \m s2 q ->
+    case n == m of
+      True  -> BLAS.dotc n p s1 q s2
+      False -> error "DOTC"
+
+-- | Euqlidean norm of vector
+vectorNorm :: BLAS1 a => Vector a -> Double
+{-# INLINE vectorNorm #-}
+vectorNorm v
+  = runST
+  $ unsafeIOToST
+  $ unsafeWithVector v $ \n s p -> BLAS.nrm2 n p s
