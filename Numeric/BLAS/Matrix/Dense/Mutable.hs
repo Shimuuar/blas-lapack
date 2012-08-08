c604c43a4fc4533cc0da7446b9c8f441ec555ddb
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2012-08-08 21:36:09 +0400

    Warnings

--- Numeric/BLAS/Matrix/Dense/Mutable.hs
+++ Numeric/BLAS/Matrix/Dense/Mutable.hs
@@ -19,9 +19,9 @@ import Control.Monad.Primitive
 
 import Data.Typeable (Typeable)
 import Data.Vector.Storable.Internal
-import qualified Data.Vector.Generic.Mutable as M
+-- import qualified Data.Vector.Generic.Mutable as M
 
-import Foreign.Ptr
+-- import Foreign.Ptr
 import Foreign.Marshal.Array ( advancePtr )
 import Foreign.ForeignPtr
 import Foreign.Storable
