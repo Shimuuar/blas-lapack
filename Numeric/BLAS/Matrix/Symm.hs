27c4f118f258603c5f46a11eb3337139c5413c84
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2012-08-08 21:53:29 +0400

    Add module for symmetric matrices

---
+++ Numeric/BLAS/Matrix/Symm.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+-- | Dense matrix
+module Numeric.BLAS.Matrix.Symm (
+    -- * Matrix data type
+    Symmetric
+  , Hermitian
+  ) where
+
+import Control.Monad.Primitive
+import Control.DeepSeq               ( NFData )
+
+import Data.Typeable                 (Typeable)
+import Data.Vector.Storable.Internal
+import qualified Data.Vector.Generic as G
+
+import Foreign.Marshal.Array ( advancePtr )
+import Foreign.Ptr
+import Foreign.ForeignPtr
+import Foreign.Storable
+
+
+----------------------------------------------------------------
+--
+----------------------------------------------------------------
+
+-- | Immutable symmetric matrix
+data Symmetric a = Symmetric {-# UNPACK #-} !Int -- order of matrix
+                             {-# UNPACK #-} !Int -- Leading dim size
+                             {-# UNPACK #-} !(ForeignPtr a)
+              deriving ( Typeable )
+
+-- | Immutable hermitian matrix
+data Hermitian a = Hermitian {-# UNPACK #-} !Int -- order of matrix
+                             {-# UNPACK #-} !Int -- Leading dim size
+                             {-# UNPACK #-} !(ForeignPtr a)
+              deriving ( Typeable )
+
