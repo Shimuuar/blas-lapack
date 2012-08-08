637b75b8b4c18f61516871b5ad2b39c37560b98c
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2012-08-09 01:46:42 +0400

    Add incomplete type class for immutable matrices

--- Numeric/BLAS/Matrix.hs
+++ Numeric/BLAS/Matrix.hs
@@ -1,3 +1,16 @@
--- | 
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts      #-}
+-- |
 -- Generic immutable matrix interface
-module Numeric.BLAS.Matrix where
+module Numeric.BLAS.Matrix (
+    IsMatrix(..)
+  ) where
+
+import Control.Monad.Primitive
+import qualified Numeric.BLAS.Matrix.Mutable as M
+import           Data.Vector.Generic (Mutable)
+
+
+class M.IsMMatrix (Mutable mat) a => IsMatrix mat a where
+  unsafeThaw   :: PrimMonad m => mat a -> m (Mutable mat (PrimState m) a)
+  unsafeFreeze :: PrimMonad m => Mutable mat (PrimState m) a -> m (mat a)
