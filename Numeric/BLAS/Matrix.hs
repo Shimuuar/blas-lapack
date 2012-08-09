2e24fb834abca89ad4de6622b35dc0ab8d9a326e
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2012-08-09 11:05:27 +0400

    Complete type class for immutable matrices and add newtype wrappers for matrix transposition and conjugate-transposition

--- Numeric/BLAS/Matrix.hs
+++ Numeric/BLAS/Matrix.hs
@@ -1,16 +1,83 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE TypeFamilies          #-}
 -- |
 -- Generic immutable matrix interface
 module Numeric.BLAS.Matrix (
+    -- * Type class
     IsMatrix(..)
+    -- * Newtype wrappers
+  , Transposed(..)
+  , Conjugated(..)
   ) where
 
+import Control.Monad             (liftM)
 import Control.Monad.Primitive
+import Data.Complex              (Complex,conjugate)
 import qualified Numeric.BLAS.Matrix.Mutable as M
 import           Data.Vector.Generic (Mutable)
 
 
+----------------------------------------------------------------
+-- Type class
+----------------------------------------------------------------
+
+-- | Basic API for immutable matrices
 class M.IsMMatrix (Mutable mat) a => IsMatrix mat a where
+  -- | Number of rows
+  rows :: mat a -> Int
+  -- | Number of columns
+  cols :: mat a -> Int
+  -- | Read element from matrix
+  unsafeIndex :: mat a -> (Int,Int) -> a
+  -- | Convert immutable matrix to mutable. Immutable matrix may not
+  --   be used after operation.
   unsafeThaw   :: PrimMonad m => mat a -> m (Mutable mat (PrimState m) a)
+  -- | Convert mutable matrix to immutable. Mutable matrix may not be
+  --   modified after operation.
   unsafeFreeze :: PrimMonad m => Mutable mat (PrimState m) a -> m (mat a)
+
+
+
+----------------------------------------------------------------
+-- Newtype wrappers
+----------------------------------------------------------------
+
+-- | Transposed matrix
+newtype Transposed mat a = Transposed { unTranspose :: mat a }
+
+type instance Mutable (Transposed mat) = M.TransposedM (Mutable mat)
+
+instance IsMatrix mat a => IsMatrix (Transposed mat) a where
+  rows (Transposed m) = cols m
+  {-# INLINE rows #-}
+  cols (Transposed m) = rows m
+  {-# INLINE cols #-}
+  unsafeIndex (Transposed m) (i,j) = unsafeIndex m (j,i)
+  {-# INLINE unsafeIndex #-}
+  unsafeThaw (Transposed m) = M.TransposedM `liftM` unsafeThaw m
+  {-# INLINE unsafeThaw #-}
+  unsafeFreeze (M.TransposedM m) = Transposed `liftM` unsafeFreeze m
+  {-# INLINE unsafeFreeze #-}
+
+
+
+-- | Conjugate-transposed matrix
+newtype Conjugated mat a = Conjugated { unConjugate :: mat a }
+
+type instance Mutable (Conjugated mat) = M.ConjugatedM (Mutable mat)
+
+instance (IsMatrix mat (Complex a), RealFloat a) => IsMatrix (Conjugated mat) (Complex a) where
+  rows (Conjugated m) = cols m
+  {-# INLINE rows #-}
+  cols (Conjugated m) = rows m
+  {-# INLINE cols #-}
+  unsafeIndex (Conjugated m) (i,j)
+    | j >= i    = unsafeIndex m (j,i)
+    | otherwise = conjugate $! unsafeIndex m (j,i)
+  {-# INLINE unsafeIndex #-}
+  unsafeThaw (Conjugated m) = M.ConjugatedM `liftM` unsafeThaw m
+  {-# INLINE unsafeThaw #-}
+  unsafeFreeze (M.ConjugatedM m) = Conjugated `liftM` unsafeFreeze m
+  {-# INLINE unsafeFreeze #-}
--- Numeric/BLAS/Matrix/Mutable.hs
+++ Numeric/BLAS/Matrix/Mutable.hs
@@ -1,16 +1,22 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE BangPatterns          #-}
 -- |
 -- Generic mutable matrix interface
 module Numeric.BLAS.Matrix.Mutable (
     -- * Type class
     IsMMatrix(..)
+    -- * Newtype wrappers
+  , TransposedM(..)
+  , ConjugatedM(..)
     -- * Element manipulation
   , read
   , write
   ) where
 
 import Control.Monad.Primitive
+import Data.Complex             (Complex,conjugate)
 import Prelude hiding (read)
 
 ----------------------------------------------------------------
@@ -33,6 +39,45 @@ class IsMMatrix mat a where
   unsafeRead :: PrimMonad m => mat (PrimState m) a -> (Int,Int) -> m a
   -- | Write element at given index
   unsafeWrite :: PrimMonad m => mat (PrimState m) a -> (Int,Int) -> a -> m ()
+
+
+----------------------------------------------------------------
+-- Newtypes
+----------------------------------------------------------------
+
+-- | Transposed matrix
+newtype TransposedM mat s a = TransposedM { unTranspose :: mat s a }
+
+instance IsMMatrix mat a => IsMMatrix (TransposedM mat) a where
+  rows (TransposedM m) = cols m
+  {-# INLINE rows #-}
+  cols (TransposedM m) = rows m
+  {-# INLINE cols #-}
+  isIndexMutable (TransposedM m) (i,j)   = isIndexMutable m (j,i)
+  {-# INLINE isIndexMutable #-}
+  unsafeRead     (TransposedM m) (i,j)   = unsafeRead     m (j,i)
+  {-# INLINE unsafeRead #-}
+  unsafeWrite    (TransposedM m) (i,j) x = unsafeWrite    m (j,i) x
+  {-# INLINE unsafeWrite #-}
+
+-- | Conjugate-transposed matrix
+newtype ConjugatedM mat s a = ConjugatedM { unConjugate :: mat s a }
+
+instance (IsMMatrix mat (Complex a), RealFloat a) => IsMMatrix (ConjugatedM mat) (Complex a) where
+  rows (ConjugatedM m) = cols m
+  {-# INLINE rows #-}
+  cols (ConjugatedM m) = rows m
+  {-# INLINE cols #-}
+  isIndexMutable (ConjugatedM m) (i,j)   = isIndexMutable m (j,i)
+  {-# INLINE isIndexMutable #-}
+  unsafeRead (ConjugatedM m) (i,j)
+    | j >= i    = unsafeRead m (j,i)
+    | otherwise = do { x <- unsafeRead m (j,i); return $! conjugate x }
+  {-# INLINE unsafeRead #-}
+  unsafeWrite (ConjugatedM m) (i,j) x
+    | j >= i    = unsafeWrite m (j,i) x
+    | otherwise = unsafeWrite m (j,i) $! conjugate x
+  {-# INLINE unsafeWrite #-}
 
 
 
